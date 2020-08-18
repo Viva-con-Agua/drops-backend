b2c01307776afb94733c00c276569fdf2f135f9f
Dennis Kleber <dennis_kleber@mailbox.org>
2020-08-18 16:26:39 +0200

    dockerfile

---
+++ install_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+go get github.com/labstack/echo/... &&
+go get github.com/gocraft/dbr/... &&
+go get github.com/go-sql-driver/mysql &&
+go get github.com/jinzhu/configor/... &&
+go get github.com/Viva-con-Agua/echo-pool/... &&
+go get github.com/go-playground/validator/... &&
+go get github.com/google/uuid/...
+
